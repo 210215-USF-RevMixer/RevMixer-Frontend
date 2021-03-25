59b740db940a578eff99b230b5fc188f4cb25f0c
jackl <jacklong34@gmail.com>
2021-03-25 17:26:14 -0400

    Please work

---
+++ AngularTone/src/app/Components/auth-button/auth-button.component.ts
@@ -0,0 +1,17 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-auth-button',
+  templateUrl: './auth-button.component.html',
+  styleUrls: ['./auth-button.component.scss']
+})
+export class AuthButtonComponent implements OnInit {
+
+  constructor(public auth: AuthService, @Inject(DOCUMENT) public document: Document) { }
+
+  ngOnInit(): void {
+  }
+
+}
