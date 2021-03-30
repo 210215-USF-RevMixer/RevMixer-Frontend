0a3004de7d4f4aad0598047b71d2ffad151e22a8
Weston Davidson <59481331+westondavidson@users.noreply.github.com>
2021-03-30 09:11:37 -0400

    added some functionality-still testing

--- AngularTone/src/app/Components/pages/upload/upload.component.ts
+++ AngularTone/src/app/Components/pages/upload/upload.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Console } from 'node:console';
 import { AmazonS3ApiService } from 'src/app/services/amazon-s3-api.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-upload',
@@ -8,32 +10,48 @@ import { AmazonS3ApiService } from 'src/app/services/amazon-s3-api.service';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
+  url: string = environment.AMAZON_REST;
+  public progress: number;
+  public message: string;
+  @Output() public onUploadFinished = new EventEmitter();
 
-  fileToUpload: any = null;
-
-  constructor(private s3Service: AmazonS3ApiService) {
-
+  constructor(private s3Service: AmazonS3ApiService, private http: HttpClient) {
+    this.progress = 0,
+    this.message = ''
    }
 
   ngOnInit(): void {
 
-
-
   }
 
+  public uploadFile = (files: any) => {
+    if (files.length === 0) {
+      return;
+    }
+    debugger;
+    let fileToUpload = <File>files[0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
 
-  uploadFileToAWS() {
-    const userId = 1;
-    this.s3Service.uploadSong(this.fileToUpload, userId).subscribe(data => {
-      console.log('file uploaded successfully');
-      
-    }, error => {
-      console.log(error);
+    this.http.post(this.url, formData, {reportProgress: true, observe: 'events'})
+    .subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress){
+        if(event.total){
+          this.progress = Math.round(100 * event.loaded / event.total);
+        }
+      }
+      else if (event.type == HttpEventType.Response) {
+        this.message = 'Upload success.';
+        if(event.body)
+        {
+        this.onUploadFinished.emit(event.body);
+        }
+      }
+
     })
+
   }
 
-  handleFileInput(event: any){
-    this.fileToUpload = event.target.files(0);
-  }
+
 
 }
