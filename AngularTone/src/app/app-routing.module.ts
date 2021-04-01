1b51ecfc61d54ea3da37c9c0e7d0691605d77f85
Soulzeon <79062042+Soulzeon@users.noreply.github.com>
2021-04-01 13:46:28 -0700

    fixed white comments

--- AngularTone/src/app/app-routing.module.ts
+++ AngularTone/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HubComponent } from './Components/pages/hub/hub.component';
 import { InstrumentComponent } from './instrument/instrument.component';
 import { ProfileComponent } from './Components/pages/profile/profile.component';
 import { AuthGuard } from '@auth0/auth0-angular';
-import { AudioPlayerComponent } from './Components/audio/audio-player/audio-player.component';
 import { UploadComponent } from './Components/pages/upload/upload.component';
 import { CreatePlaylistComponent } from './Components/pages/create-playlist/create-playlist.component';
 import { ViewPlaylistComponent } from './Components/pages/view-playlist/view-playlist.component';
@@ -14,7 +13,6 @@ const routes: Routes = [
   {path: '', component: InstrumentComponent},
   {path: 'hub', component: HubComponent, canActivate: [AuthGuard]},
   {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
-  {path: 'test', component: AudioPlayerComponent, canActivate: [AuthGuard]},
   {path: 'upload', component: UploadComponent, canActivate: [AuthGuard]},
   {path: 'newPlayList', component: CreatePlaylistComponent, canActivate: [AuthGuard]},
   {path: 'viewPlaylist', component: ViewPlaylistComponent, canActivate: [AuthGuard]},
