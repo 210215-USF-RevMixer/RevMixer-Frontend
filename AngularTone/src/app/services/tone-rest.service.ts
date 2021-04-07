be09a086f9ec49c36634a936033dddc6bbac0dc4
Jacob Gaughan <jakegaughan5@gmail.com>
2021-04-06 22:49:03 -0500

    refactored

--- AngularTone/src/app/services/tone-rest.service.ts
+++ AngularTone/src/app/services/tone-rest.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpHeaders, HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '../Models/user';
+import { User } from '../Models/User';
 @Injectable({
   providedIn: 'root'
 })
