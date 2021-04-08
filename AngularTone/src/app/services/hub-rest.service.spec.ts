31bc18b4d99677c15036772b86018fa74542ae48
Matt Hunt <wmatthunt@gmail.com>
2021-04-08 16:57:28 -0400

    get alls for hub and uploaded music work

--- AngularTone/src/app/services/hub-rest.service.spec.ts
+++ AngularTone/src/app/services/hub-rest.service.spec.ts
@@ -1,16 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+
+import { TestBed, inject } from '@angular/core/testing';
 
 import { HubRestService } from './hub-rest.service';
 
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+
 describe('HubRestService', () => {
-  let service: HubRestService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HubRestService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HubRestService]
+    });
   });
+  it(
+    'should get uploaded music',
+    inject(
+      [HttpTestingController, HubRestService],
+      (httpMock: HttpTestingController, hubRestService: HubRestService) => {
+        const mockUploads = [
+          { name: 'Upload1', likes: 1 },
+          { name: 'Upload2', likes: 0 }
+        ];
+        hubRestService.GetAllUpload().subscribe((event: HttpEvent<any>) => {
+          switch (event.type) {
+            case HttpEventType.Response:
+              expect(event.body).toEqual(mockUploads);
+          }
+        });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        const mockReq = httpMock.expectOne(hubRestService.url);
+
+        expect(mockReq.cancelled).toBeFalsy();
+        expect(mockReq.request.responseType).toEqual('json');
+        mockReq.flush(mockUploads);
+
+        httpMock.verify();
+      }
+    ));
 });
--- AngularTone/src/app/services/uploaded-music-rest.service.spec.ts
+++ AngularTone/src/app/services/uploaded-music-rest.service.spec.ts
@@ -1,16 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+
+import { TestBed, inject } from '@angular/core/testing';
 
 import { UploadedMusicRestService } from './uploaded-music-rest.service';
 
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+
 describe('UploadedMusicRestService', () => {
-  let service: UploadedMusicRestService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UploadedMusicRestService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UploadedMusicRestService]
+    });
   });
+  it(
+    'should get uploaded music',
+    inject(
+      [HttpTestingController, UploadedMusicRestService],
+      (httpMock: HttpTestingController, uploadedMusicRestService: UploadedMusicRestService) => {
+        const mockUploads = [
+          { name: 'Upload1' },
+          { name: 'Upload2' }
+        ];
+        uploadedMusicRestService.GetUploadedSongs().subscribe((event: HttpEvent<any>) => {
+          switch (event.type) {
+            case HttpEventType.Response:
+              expect(event.body).toEqual(mockUploads);
+          }
+        });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        const mockReq = httpMock.expectOne(uploadedMusicRestService.url);
+
+        expect(mockReq.cancelled).toBeFalsy();
+        expect(mockReq.request.responseType).toEqual('json');
+        mockReq.flush(mockUploads);
+
+        httpMock.verify();
+      }
+    ));
 });
