3db0fcb4ba32173368c36d2d6d5fdcb058c4f34c
Weston Davidson <59481331+westondavidson@users.noreply.github.com>
2021-04-08 14:57:11 -0400

    added sample upload page - methods appear to be working as desired

--- AngularTone/src/app/app-routing.module.ts
+++ AngularTone/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from '@auth0/auth0-angular';
 import { UploadComponent } from './Components/pages/upload/upload.component';
 import { CreatePlaylistComponent } from './Components/pages/create-playlist/create-playlist.component';
 import { ViewPlaylistComponent } from './Components/pages/view-playlist/view-playlist.component';
+import { UploadSampleComponent } from './Components/pages/upload-sample/upload-sample.component';
 
 
 const routes: Routes = [
@@ -14,9 +15,11 @@ const routes: Routes = [
   {path: 'hub', component: HubComponent, canActivate: [AuthGuard]},
   {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
   {path: 'upload', component: UploadComponent, canActivate: [AuthGuard]},
+  {path: 'uploadSample', component: UploadSampleComponent, canActivate: [AuthGuard]},
   {path: 'newPlayList', component: CreatePlaylistComponent, canActivate: [AuthGuard]},
   {path: 'viewPlaylist', component: ViewPlaylistComponent, canActivate: [AuthGuard]},
   {path: '**', component: InstrumentComponent}
+
 
 ];
 
