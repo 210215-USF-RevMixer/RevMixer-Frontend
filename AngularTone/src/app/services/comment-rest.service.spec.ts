51408ff5b1643cac8cacf24023399e7072368f14
STEVENPC\Steven <stevenimas11@hotmail.com>
2021-04-08 23:52:47 -0400

    addSampleSet and GetAllSampleSets tests

--- AngularTone/src/app/services/comment-rest.service.spec.ts
+++ AngularTone/src/app/services/comment-rest.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { Comments } from '../Models/Comments';
+import { User } from '../Models/User';
 
 import { CommentRestService } from './comment-rest.service';
 
@@ -13,4 +15,48 @@ describe('CommentRestService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('GetAllComment should return observable comments ', () => {
+    service.GetAllComment().subscribe(result => {
+          expect(result instanceof Object).toBeTruthy();
+    } );
+ });
+
+
+ it('SubmitComment should return comment ', () => {
+   let user: User = {id: 1,
+    userName: 'string',
+    email: 'string',
+    isAdmin: false,
+
+    userProjects: [],
+    sample: [],
+    comments: [],
+    uploadMusics: [],
+    playlists: []};
+  let newComment : Comments = { 
+    id:1,
+    comment: 'testComment',
+    commentData:new Date(),
+    userId: 1,
+    uploadMusicId: 1,
+    user: user,
+    uploadMusic:{id: 1,
+      userId: 1,
+      musicFilePath: 'string',
+      name: 'string',
+      uploadDate:new  Date(),
+      likes: 1,
+      plays: 1,
+      isPrivate: false,
+  
+      user: user,
+  
+      musicPlaylists: [],
+      comments: []}
+  };
+   service.SubmitComment(newComment).subscribe(result => {
+     expect (result instanceof (Object )). toBeTruthy();
+     expect(result.comment).toEqual(newComment.comment);
+   });
+ });
 });
--- AngularTone/src/app/services/sample-set.service.spec.ts
+++ AngularTone/src/app/services/sample-set.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
+import { SampleSets } from '../Models/SampleSets';
+import {User} from '../Models/User'; 
 import { SampleSetService } from './sample-set.service';
 
 describe('SampleSetService', () => {
@@ -13,4 +14,40 @@ describe('SampleSetService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('GetAllSampleSets should return any observable', () => {
+    service.GetAllSampleSets().subscribe(result => {
+      expect(result instanceof Object).toBeTruthy();
+    });
+
+
+
+    it('AddSampleSet should return any Observable', () => {
+      let user: User = {id: 1,
+        userName: 'string',
+        email: 'string',
+        isAdmin: false,
+    
+        userProjects: [],
+        sample: [],
+        comments: [],
+        uploadMusics: [],
+        playlists: []};
+
+      let sampleSet : SampleSets={
+        id: 1,
+        name: 'string',
+        userId: 1,
+        isPrivate: false,
+        user: user,
+        samples: [],
+      }
+      service.AddSampleSet(sampleSet).subscribe (result =>{
+        expect (result instanceof (Object )). toBeTruthy();
+      });
+    });
+    
+  });
+
+  
 });
