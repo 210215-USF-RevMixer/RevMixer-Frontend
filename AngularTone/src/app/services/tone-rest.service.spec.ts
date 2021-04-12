0bd753bc65c0d9bc59d74460ee54b9288ce01b50
madcyn <76539571+MadCynWil@users.noreply.github.com>
2021-04-12 13:00:46 -0700

    Update tone-rest.service.spec.ts

--- AngularTone/src/app/services/tone-rest.service.spec.ts
+++ AngularTone/src/app/services/tone-rest.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-
+import { User } from '../Models/User';
 import { ToneRESTService } from './tone-rest.service';
+import { asyncData } from '../../testHelpers/observables';
 
 describe('ToneRESTService', () => {
   let service: ToneRESTService;
-
+  let httpClientSpy: { get: jasmine.Spy }
+  let user: User[]= [{ id: 0, userName: "mads", email:"x@x.com", isAdmin: true, userProjects: [],sample:[],comments:[], uploadMusics:[],playlists:[]}];
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ToneRESTService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new ToneRESTService(httpClientSpy as any);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
+  it('should Get All Users', () => {
+    httpClientSpy.get.and.returnValue(asyncData(user));
+    service.getAllUsers().subscribe(
+      projs =>
+        expect(projs).toEqual(user),
+      fail);
+
+    expect(httpClientSpy.get.calls.count()).toBe(1);
+
+  });
+
+
+
 });
