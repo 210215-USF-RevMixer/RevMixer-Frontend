1907f3533fa9bc49063a387b744bd8ee66764197
KMurphy-5 <kevincitorios@gmail.com>
2021-04-16 09:04:55 -0700

    Change to the add sampleset

--- AngularTone/src/app/Components/pages/create-sample-set/create-sample-set.component.ts
+++ AngularTone/src/app/Components/pages/create-sample-set/create-sample-set.component.ts
@@ -74,9 +74,15 @@ export class CreateSampleSetComponent implements OnInit {
  
     
   }
-
+  
+    
   onSubmit(): void { 
-    this.setsService.AddSampleSet(this.newSampleSet,this.user.id);
+    const formData = new FormData();
+    formData.append('userId',(this.newUsersSampleSet.userId).toString());
+    formData.append('name', this.newSampleSet.name);
+    
+    
+    this.setsService.AddSampleSet(formData);
     
     alert(`${this.newSampleSet.name} added to your sample sets!`);
     this.router.navigate(['profile']);
