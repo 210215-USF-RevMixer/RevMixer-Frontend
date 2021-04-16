7d7762b91c26c8fffede63d8a0b9a02a1d51a056
KMurphy-5 <kevincitorios@gmail.com>
2021-04-16 11:46:34 -0700

    Update sample.service.ts

--- AngularTone/src/app/services/sample.service.ts
+++ AngularTone/src/app/services/sample.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import * as Tone from 'tone';
-import { Sample } from '../Models/Sample';
 import { SampleSetService } from './sample-set.service';
 
 @Injectable({
