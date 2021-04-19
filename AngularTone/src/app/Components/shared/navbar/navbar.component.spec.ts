f88c37e7cd76548af9278a6dfc848121b12d3106
madcyn <76539571+MadCynWil@users.noreply.github.com>
2021-04-19 07:49:43 -0700

    fix some test issues

--- AngularTone/src/app/Components/shared/navbar/navbar.component.spec.ts
+++ AngularTone/src/app/Components/shared/navbar/navbar.component.spec.ts
@@ -1,22 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AuthService } from '@auth0/auth0-angular';
 import { NavbarComponent } from './navbar.component';
-
+export class MockAuthService {
+  public handleAuth(): void {
+    return;
+  }
+}
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+
+   // let aSpy = jasmine.createSpyObj('AuthService', []);
+    // Provide the dummy/mock data to sortNumberData method.
+   
+    const authMock = {
+      user$: {client:{email: 'someValue'} }};
+    /*await TestBed.configureTestingModule({
+      declarations: [NavbarComponent],
+      providers:
+        [{ provider: AuthService, useValue: authMock}]
+       
     })
-    .compileComponents();
+    .compileComponents();*/
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    const authMock = {
+      user$: { client: { email: 'someValue' } },
+       auth0Client: {},
+      configFactory: {},
+      location: null,
+      navigator: {},
+      isLoadingSubject$: {},
+      errorSubject$: {},
+      refreshState$: {},
+      ngUnsubscribe$: {},
+      isLoading$: false,
+      isAuthenticatedTrigger$: {},
+      isAuthenticated$: {},
+      idTokenClaims$: {},
+      error$: {},
+      ngOnDestroy: {},
+      loginWithRedirect: {},
+      loginWithPopup: {},
+      logout: {},
+      getAccessTokenSilently: {},
+      getAccessTokenWithPopup: {},
+      shouldHandleCallback: {},
+      handleRedirectCallback: {}
+
+
+    };
+    //fixture = TestBed.createComponent(NavbarComponent);
+    let userService = {};
+    component = new NavbarComponent(authMock,userService);
+    //fixture.detectChanges();
   });
 
   // it('should create', () => {
