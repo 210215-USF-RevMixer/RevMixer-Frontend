2bb21949626989f5102a093295be20a491f7c609
Soulzeon <79062042+Soulzeon@users.noreply.github.com>
2021-04-20 12:56:35 -0700

    love changing this kamra file

--- AngularTone/karma.conf.js
+++ AngularTone/karma.conf.js
@@ -28,14 +28,10 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/AngularTone'),
       subdir: '.',
       reporters: [
-        { type: 'lcov' },
-        { type: 'text-summary' }
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: "lcov" }
       ]
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly','text-summary', 'cobertura'],
-      fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -47,3 +43,70 @@ module.exports = function (config) {
     restartOnFileChange: true
   });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Karma configuration file, see link for more information
+// // https://karma-runner.github.io/1.0/config/configuration-file.html
+
+// module.exports = function (config) {
+//   config.set({
+//     basePath: '',
+//     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+//     plugins: [
+//       require('karma-jasmine'),
+//       require('karma-chrome-launcher'),
+//       require('karma-jasmine-html-reporter'),
+//       require('karma-coverage'),
+//       require('@angular-devkit/build-angular/plugins/karma')
+//     ],
+//     client: {
+//       jasmine: {
+//         // you can add configuration options for Jasmine here
+//         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+//         // for example, you can disable the random execution with `random: false`
+//         // or set a specific seed with `seed: 4321`
+//       },
+//       clearContext: false // leave Jasmine Spec Runner output visible in browser
+//     },
+//     jasmineHtmlReporter: {
+//       suppressAll: true // removes the duplicated traces
+//     },
+//     coverageReporter: {
+//       dir: require('path').join(__dirname, './coverage/AngularTone'),
+//       subdir: '.',
+//       reporters: [
+//         { type: 'lcov' },
+//         { type: 'text-summary' }
+//       ]
+//     },
+//     coverageIstanbulReporter: {
+//       dir: require('path').join(__dirname, '../../coverage'),
+//       reports: ['html', 'lcovonly','text-summary', 'cobertura'],
+//       fixWebpackSourcePaths: true,
+//     },
+//     reporters: ['progress', 'kjhtml'],
+//     port: 9876,
+//     colors: true,
+//     logLevel: config.LOG_INFO,
+//     autoWatch: true,
+//     browsers: ['Chrome'],
+//     singleRun: false,
+//     restartOnFileChange: true
+//   });
+// };
